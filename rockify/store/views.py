fb10f9ec3e745fe18ff15e055b439ce527bc5aeb
dreevo <sidedark85@gmail.com>
2020-09-27 13:03:28 +0100

    Changed some styling and configured the database

--- rockify/store/views.py
+++ rockify/store/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render
+from .models import *
 
 
 def store(request):
-    return render(request, 'store/store.html', {})
+    products = Product.objects.all()
+    context = {
+        "products": products
+    }
+    return render(request, 'store/store.html', context)
 
 
 def cart(request):
-    return render(request, 'store/cart.html', {})
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    context = {'items': items, 'order': order}
+    return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-    return render(request, 'store/checkout.html', {})
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    context = {'items': items, 'order': order}
+    return render(request, 'store/checkout.html', context)
