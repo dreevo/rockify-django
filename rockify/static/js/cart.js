6d90f0345327c9c8c703525d08e4eec7dc032a01
dreevo <sidedark85@gmail.com>
2020-09-28 11:35:35 +0100

    Added guest cart functionnality and csrf fix

--- rockify/static/js/cart.js
+++ rockify/static/js/cart.js
@@ -4,12 +4,31 @@ for (i = 0; i < updateBtns.length; i++) {
     var productId = this.dataset.product;
     var action = this.dataset.action;
     if (user == "AnonymousUser") {
-      console.log("user is not logged in..");
+      addCookieItem(productId, action);
     } else {
       updateUserOrder(productId, action);
     }
   });
 }
+
+const addCookieItem = (productId, action) => {
+  if (action == "add") {
+    if (cart[productId] == undefined) {
+      cart[productId] = { quantity: 1 };
+    } else {
+      cart[productId]["quantity"] += 1;
+    }
+  }
+  if (action == "remove") {
+    cart[productId]["quantity"] -= 1;
+    if (cart[productId]["quantity"] <= 0) {
+      delete cart[productId];
+    }
+  }
+  document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/";
+  location.reload();
+  console.log(cart);
+};
 
 const updateUserOrder = (productId, action) => {
   var url = "/update_item/";
